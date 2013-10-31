2345a9f2178cbaab98a5656a25212380e6e37548
Pierre Rudloff <contact@rudloff.pro>
2013-10-31 22:19:01 +0000

    App manifest + lint

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 /*global OpenLayers*/
 /*jslint browser: true */
-var map = new OpenLayers.Map("mapdiv");
-map.addLayer(new OpenLayers.Layer.OSM());
+window.addEventListener('load', function () {
+    'use strict';
+    var map = new OpenLayers.Map("mapdiv"), lonLat = new OpenLayers.LonLat(7.750576, 48.58476).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()), zoom = 15, markers = new OpenLayers.Layer.Markers(), client = new XMLHttpRequest();
+    map.addLayer(new OpenLayers.Layer.OSM());
 
-var lonLat = new OpenLayers.LonLat(7.750576, 48.58476).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+    map.addLayer(markers);
 
-var zoom = 15;
+    map.setCenter(lonLat, zoom);
 
-var markers = new OpenLayers.Layer.Markers();
-map.addLayer(markers);
-
-map.setCenter(lonLat, zoom);
-
-function handler(e) {
-    "use strict";
-    if (e.target.readyState === e.target.DONE) {
-        if (e.target.status === 200 && e.target.response !== null) {
-            // success!
-            var nodes, i, lonLat;
-            nodes = JSON.parse(e.target.response);
-            for (i = 0; i < nodes.length; i += 1) {
-                lonLat = new OpenLayers.LonLat(nodes[i].long, nodes[i].lat);
-                markers.addMarker(new OpenLayers.Marker(lonLat.transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject())));
-                map.addPopup(new OpenLayers.Popup.FramedCloud(null, lonLat, null, "<b>" + nodes[i].name + "</b><br/>" + nodes[i].current + "/" + nodes[i].max, null, false));
+    function handler(e) {
+        if (e.target.readyState === e.target.DONE) {
+            if (e.target.status === 200 && e.target.response !== null) {
+                // success!
+                var nodes, i;
+                nodes = JSON.parse(e.target.response);
+                for (i = 0; i < nodes.length; i += 1) {
+                    lonLat = new OpenLayers.LonLat(nodes[i].long, nodes[i].lat);
+                    markers.addMarker(new OpenLayers.Marker(lonLat.transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject())));
+                    map.addPopup(new OpenLayers.Popup.FramedCloud(null, lonLat, null, "<b>" + nodes[i].name + "</b><br/>" + nodes[i].current + "/" + nodes[i].max, null, false));
+                }
             }
         }
     }
-}
 
-var client = new XMLHttpRequest();
-client.onreadystatechange = handler;
-client.open("GET", "getJSON.php");
-client.send();
 
+    client = new XMLHttpRequest();
+    client.onreadystatechange = handler;
+    client.open("GET", "getJSON.php");
+    client.send();
+}, false);
 
